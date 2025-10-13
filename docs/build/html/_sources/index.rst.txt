3b3ccb233461d888b1af59a8469960a4b4ad60a2
ZuhangZHANG <zuhangzhang@163.com>
2025-10-14 03:14:33 +0800

    fix: remove embedded .git in docs/

---
+++ docs/build/html/_sources/index.rst.txt
@@ -0,0 +1,25 @@
+.. RMC documentation master file, created by
+   sphinx-quickstart on Mon Oct 13 23:06:04 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to RMC's documentation!
+===============================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+   
+   introduction
+   socio_econ
+   ref_ener_sys
+   rmc_steel
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ docs/source/conf.py
@@ -0,0 +1,69 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'RMC'
+author = 'School of Applied Economics, Renmin University of China'
+copyright = f'2025, {author}'
+release = 'test'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'myst_parser',       # 支持 Markdown
+    'sphinx_markdown_tables', # 支持 Markdown 表格
+    'sphinx.ext.mathjax', # 数学公式
+]
+
+myst_enable_extensions = [
+    "amsmath",    # 支持LaTeX数学环境（如 \begin{align}）
+    "dollarmath", # 支持美元符号数学（如 $...$ 和 $$...$$）
+]
+
+# 确保使用 MathJax 3.x 的路径
+mathjax_path = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"
+
+mathjax3_config = {
+    'tex': {
+        'inlineMath': [['$', '$'], ['\\(', '\\)']],
+        'displayMath': [['$$', '$$'], ['\\[', '\\]']],
+        'processEscapes': True,
+    },
+}
+
+# 指定哪些文件后缀会被解析
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
+
+
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# mathjax_path = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+
+import sys
+from pathlib import Path
+
+# 获取当前文件(conf.py)的路径
+current_dir = Path(__file__).resolve().parent
+# 计算项目根目录的路径（假设项目根目录在 docs 的上一级）
+project_root = current_dir.parent.parent
+# 将项目根目录添加到 sys.path
+sys.path.insert(0, str(project_root))
+# # 如果你的模型代码在项目下的某个子目录，例如 'src'，也可以单独添加
+# sys.path.insert(0, str(project_root / 'src'))
